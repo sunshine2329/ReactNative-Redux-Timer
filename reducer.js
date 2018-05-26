35adb7e9dff3763853949be3a4fceb0e443d0bad
sunshine2329 <seniordeveloper00001@gmail.com>
2018-05-26 04:33:40 -0400

    Connecting the Components to State

--- reducer.js
+++ reducer.js
@@ -44,6 +44,8 @@ function reducer(state = initialState, action) {
             return applyRestartTimer(state);
         case ADD_SECOND:
             return applyAddSecond(state);
+        default:
+            return state;
     }
 }
 
