5ceb1b765b42d745695b806da42261f77885b086
sunshine2329 <seniordeveloper00001@gmail.com>
2018-05-25 22:43:13 -0400

    Timer component with Text

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Timer from './components/Timer';
+
+export default class App extends React.Component {
+  render() {
+    return <Timer />
+  }
+}
